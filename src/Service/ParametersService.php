fd7f797d47ce0f824751d355235fdc984be3791f
tzepart <tzepart@gmail.com>
2018-07-26 18:58:27 +0300

    w_add_sParametersService

---
+++ src/Service/ParametersService.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artem
+ * Date: 26/07/2018
+ * Time: 18:52
+ */
+
+namespace Service;
+
+use Symfony\Component\Yaml\Yaml;
+
+
+/**
+ * Class ParametersService
+ * @package Service
+ */
+class ParametersService
+{
+
+    /**
+     * @var array
+     */
+    private $ymalParseArray;
+
+    /**
+     * AuthService constructor.
+     */
+    public function __construct()
+    {
+        $this->ymalParseArray = Yaml::parseFile(__DIR__.'/../app/parameters.yml');
+    }
+
+    /**
+     * @param string $parameterName
+     * @return string|null
+     */
+    public function getParameterByName(string $parameterName)
+    {
+        if(isset($this->ymalParseArray['parameters'][$parameterName])){
+            return $this->ymalParseArray['parameters'][$parameterName];
+        }else{
+            return null;
+        }
+    }
+
+
+}
