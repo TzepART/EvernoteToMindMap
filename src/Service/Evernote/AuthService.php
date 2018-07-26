ca5840401f7f57eafaff8d9de9d61b1f6763ce95
tzepart <tzepart@gmail.com>
2018-07-26 19:11:23 +0300

    w_add_AuthService

---
+++ src/Service/Evernote/AuthService.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artem
+ * Date: 26/07/2018
+ * Time: 18:50
+ */
+
+namespace Service\Evernote;
+
+/**
+ * Class AuthService
+ * @package Service\Evernote
+ */
+class AuthService
+{
+
+}
