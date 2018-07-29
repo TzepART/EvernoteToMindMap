a42ba237e624c4dff1dc2f3498a1ec02c2eedb06
tzepart <tzepart@gmail.com>
2018-07-29 03:25:58 +0300

    w_fix

---
+++ src/Service/NoteLinksServiceInterface.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artem
+ * Date: 29/07/2018
+ * Time: 03:13
+ */
+
+namespace Service;
+
+use Model\Note\NoteLinksListInterface;
+
+
+/**
+ * Interface NoteLinksInterface
+ * @package Service
+ */
+interface NoteLinksServiceInterface
+{
+    /**
+     * @return NoteLinksListInterface
+     */
+    public function getNoteLinksList() : NoteLinksListInterface;
+
+    /**
+     * @return mixed
+     */
+    public function initNoteLinksList();
+}
